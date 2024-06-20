81001fc9078c93e70428078b4795f071cb9a39db
Ismaël PAINCHAN <painch_i@etna-alternance.net>
2024-06-20 13:11:54 +0200

    chore: Suppression otp format :'(

--- backend/src/domain/mailing/mails.manager.ts
+++ backend/src/domain/mailing/mails.manager.ts
@@ -1,5 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import type { Options } from 'boxen';
 import { FeatureFlagsService } from '../../infrastructure/feature-flags/feature-flags.service';
 import { IFeatureFlagsService } from '../_shared/feature-flags-service.interface';
 
@@ -13,32 +12,13 @@ export class MailsManager {
     const sendOTPEmailEnabled =
       this.featureFlagsService.isEnabled('send-otp-emails');
     if (!sendOTPEmailEnabled) {
-      const formattedOtp = await formatOtp(email, otp);
-      console.log(formattedOtp);
+      console.log(
+        `\x1b[32m%s\x1b[0m`,
+        `✅ OTP for \x1b[34m${email}\x1b[0m: \x1b[33m${otp}\x1b[0m`,
+      );
     } else {
       // Send email
       console.warn('No email implementation yet');
     }
   }
-}
-
-async function formatOtp(email: string, otp: string) {
-  const boxen = await import('boxen');
-  const { default: chalk } = await import('chalk');
-  const otpMessage = `
-  ${chalk.blue.bold('🔐 OTP Information')}
-  ${chalk.blue('Email:')} ${chalk.white.bold(email)}
-  ${chalk.blue('OTP:')} ${chalk.green.bold(otp)}
-  `;
-
-  const boxenOptions: Options = {
-    padding: 1,
-    margin: 1,
-    borderStyle: 'round',
-    borderColor: 'blue',
-    backgroundColor: '#555555',
-  };
-
-  const formattedMessage = boxen.default(otpMessage, boxenOptions);
-  return formattedMessage;
 }
