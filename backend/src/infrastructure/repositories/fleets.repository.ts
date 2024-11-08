c0c6b73bd01e8518c89286a185f9298c1ad0e903
Ismaël PAINCHAN <painch_i@etna-alternance.net>
2024-11-08 10:54:46 +0100

    Correction current fleet query

--- backend/src/infrastructure/repositories/fleets.repository.ts
+++ backend/src/infrastructure/repositories/fleets.repository.ts
@@ -223,7 +223,7 @@ export class FleetsRepository implements IFleetsRepository {
       where = {
         ...where,
         status: {
-          not: FleetStatusToDatabase[FleetStatus.ARRIVED, FleetStatus.CANCELLED],
+          notIn: [FleetStatusToDatabase[FleetStatus.ARRIVED], FleetStatusToDatabase[FleetStatus.CANCELLED]],
         },
       };
     }
