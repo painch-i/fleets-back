6f081ddcfc385039a007ae5b3a4188a0ae63b342
Ismaël PAINCHAN <painch_i@etna-alternance.net>
2024-12-03 12:54:34 +0100

    Update package.json, render.yaml, and config.variables.ts; remove unused Stations.modal component

--- mobile-app/src/config/config.variables.ts
+++ mobile-app/src/config/config.variables.ts
@@ -4,12 +4,13 @@ import { Capacitor } from '@capacitor/core';
 const LAN_SERVER_IP = '192.168.1.21';
 const LOCAL_SERVER_IP = 'localhost';
 const LOCAL_SERVER_PORT = 3011;
+const PROD_BACKEND_DOMAIN = 'api.fleets-app.com';
 
 function getConfigUrl() {
   const isNativePlatform = Capacitor.isNativePlatform();
   const localServerIp = isNativePlatform ? LAN_SERVER_IP : LOCAL_SERVER_IP;
-  let BACKEND_URL = 'https://api.fleets-app.com';
-  let SOCKET_URL = 'wss://api.fleets-app.com';
+  let BACKEND_URL = `https://${PROD_BACKEND_DOMAIN}`;
+  let SOCKET_URL = `wss://${PROD_BACKEND_DOMAIN}`;
 
   if (isEnvFlagTrue('VITE_LOCAL_SERVER')) {
     BACKEND_URL = `http://${localServerIp}:${LOCAL_SERVER_PORT}`;
