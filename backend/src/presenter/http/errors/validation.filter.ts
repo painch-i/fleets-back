37d47d18a1924800f866553a74c3eef967c5b84e
Ismaël PAINCHAN <painch_i@etna-alternance.net>
2024-12-10 17:06:52 +0100

    Add RowNotFoundFilter to handle PrismaClientKnownRequestError and improve validation error handling

--- backend/src/presenter/http/errors/validation.filter.ts
+++ backend/src/presenter/http/errors/validation.filter.ts
@@ -1,4 +1,11 @@
-import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { ZodError } from 'zod';
 import { ValidationError } from '../../../domain/_shared/errors/validation.error';
 
@@ -17,6 +24,29 @@ export class ValidationFilter implements ExceptionFilter {
       statusCode: 400,
       message: 'Bad Request',
       error: exception,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
+
+@Catch(PrismaClientKnownRequestError)
+export class RowNotFoundFilter implements ExceptionFilter {
+  private logger = new Logger(RowNotFoundFilter.name);
+
+  catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
+    if (host.getType() !== 'http') {
+      this.logger.error(exception, exception.stack);
+      throw exception;
+    }
+    if (exception.code !== 'P2025') {
+      throw exception;
+    }
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    response.status(HttpStatus.BAD_REQUEST).json({
+      statusCode: HttpStatus.BAD_REQUEST,
+      message: 'Bad Request',
+      error: exception.message,
       timestamp: new Date().toISOString(),
     });
   }
